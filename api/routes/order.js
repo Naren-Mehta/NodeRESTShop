3dde41c4b6a21d317e5da13d00767693cfd979ea
Narendra Singh Mehta <NarendraM@quinnox.com>
2019-09-23 17:37:00 +0530

    Working on ORder

--- api/routes/order.js
+++ api/routes/order.js
@@ -1,5 +1,27 @@
 const express= require('express');
 
-const route= express.Router();
+const router= express.Router();
+
+router.get('/',(req, res, next) => {
+    res.status(200).json({
+        message:'List of all orders'
+    });
+});
+
+router.get('/:orderId',(req, res, next) => {
+    const orderId= req.params.orderId;
+    res.status(200).json({
+        message:'Selected Order Id is :'+orderId
+    });
+});
+
+router.delete('/:orderId',(req, res, next) => {
+    const orderId= req.params.orderId;
+    res.status(200).json({
+        message:'Selected Order Id  :'+orderId + ' is deleted'
+    });
+});
+
+module.exports=router;
 
 
